08cc3be3047a96bbb297e64815d1c4af9e138259
Matthew Weser <matthew.weser@gmail.com>
2022-11-02 03:46:56 -0700

    Try parse with own string

--- python-utils/mc_util/run_mob.py
+++ python-utils/mc_util/run_mob.py
@@ -33,5 +33,11 @@ def parse_b64_receipt(b64_string):
 
     return full_service_receipt
 
+result = parse_b64_receipt(
+    'CiIKIBRB19Uul7i2HivDzB8fMCOLbgAlnTq+cRbJF2KUSEI0EiIKIDhFYUymnMjt47yuEDj8x5zjsk402vyUMIgHPObC4gORGI/BNyI3CiIKICag6rwwgjGQ6Nm9zMol/2WEzaaLW5l3l6MX7pm4VK5LEcW6zicsFO8jGgiZG6Ak6hTstg==')
+
+result_str = str(result)
+print(result_str.replace("'", "\""))
+
 
 print(parse_b64_receipt(args[1]))
